4bb9588184c59dbb8f8d9871442592c72b691d11
mopore <jni@mopore.org>
2025-08-02 15:44:15 +0200

    Code clean up for readability

--- bun/imageIdent/main_local.ts
+++ bun/imageIdent/main_local.ts
@@ -1,7 +1,14 @@
 import {Ollama} from 'ollama';
 import imgb64 from 'image-to-base64';
 
-const ollama = new Ollama({host:"host.docker.internal"});
+// LOCALHOST = "localhost" as const;
+// ARC_VM_HOST_WG_IP = "10.200.0.2" as const; // IP of wg.PdJniRegainer
+// ARC_VM_HOST_HOME_IP = 192.168.199.183
+// ARC_VM_INTERNAL_IP = "192.168.68.2" as const;
+// JNIMACSTUDIO_IP = "10.200.0.11" as const;  // IP of wg.jnimacstudio
+//
+const ollama = new Ollama({host:"10.200.0.2"});
+
 // const ollama = new Ollama({host:"192.168.199.246"});
 // const prompt = "Describe this image in one sentence.";
 const prompt = "Answer with 'YES' or 'NO' if an animal is present in the image.";
